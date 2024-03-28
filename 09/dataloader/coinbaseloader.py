ae32a31046b74bda84fd3ed485ba098f38287cda
Evil Moo <moo@yovbak.com>
2024-03-28 09:02:40 +0200

    Lab 9, pydantic
    Signed-off-by: Evil Moo <moo@yovbak.com>

---
+++ 09/dataloader/coinbaseloader.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+from enum import Enum
+
+from .baseloader import BaseDataLoader
+
+import logging
+
+class Granularity(Enum):
+    ONE_MINUTE=60,
+    FIVE_MINUTES=300,
+    FIFTEEN_MINUTES=900,
+    ONE_HOUR=3600,
+    SIX_HOURS=21600,
+    ONE_DAY=86400
+
+class CoinbaseLoader(BaseDataLoader):
+
+    def __init__(self, endpoint="https://api.exchange.coinbase.com"):
+        super().__init__(endpoint)
+        self._logger = logging.getLogger("COINBASE")
+        self._logger.info("created")
+
+    def get_pairs(self) -> dict[str, any]:
+        self._logger.debug("get pairs")
+        data = self._get_req("/products")
+        return json.loads(data)
+
+    def get_stats(self, pair: str) -> dict[str, any]:
+        self._logger.debug(f"get pair {pair} stats")
+        data = self._get_req(f"/products/{pair}")
+        return json.loads(data)
+
+    def get_historical_data(self, pair: str, begin: datetime, end: datetime, granularity: Granularity) -> dict[str, any]:
+        self._logger.debug(f"get pair {pair} history")
+        params = {
+            "start": begin,
+            "end": end,
+            "granularity": granularity.value
+        }
+        # retrieve needed data from Coinbase
+        data = self._get_req("/products/" + pair + "/candles", params)
+        # parse response and create DataFrame from it
+        return json.loads(data),
+
+if __name__ == "__main__":
+    loader = CoinbaseLoader()
+    data = loader.get_pairs()
+    print(data)
+    data = loader.get_stats("btc-usdt")
+    print(data)
+    data = loader.get_historical_data("btc-usdt", "2023-01-01", "2023-06-30", granularity=Granularity.ONE_DAY)
+    print(data.head(5))
